667526790d08af0fe8d2ed776a8bd51564e3fefd
dani24gh <19047@portalcedi.edu.mx>
2025-03-25 08:18:02 -0600

    lol

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,15 @@
-
-import { provideAuth, getAuth } from '@angular/fire/auth';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from './environments/environment';
 
-
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { environment } from './environments/environment';
-
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-
-import { provideAuth, getAuth } from '@angular/fire/auth';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-if (environment.production) {
-  enableProdMode();
-}
-
 bootstrapApplication(AppComponent, {
   providers: [
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-<<<<<<< HEAD
-    // Agregar Firebase
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-=======
-    provideIonicAngular(),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
->>>>>>> 82a096698bc9d7924026010bf3aee62d37ff338b
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
+
